6f91c8e08f6e1329e0fd99a3f746ba7729dd60bc
Diego Borges <drborges.cic@gmail.com>
2016-10-16 12:05:56 -0200

    Remove list view direction property

--- app/components/ListView.jsx
+++ app/components/ListView.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 
-let direction2class = {
-  'vertical': 'flex-column',
-  'horizontal': 'flex-row',
-}
-
 const ListView = (props) => {
-  let direction = props.direction || 'vertical'
-  let classes = `list-view flex ${direction2class[direction]} ${props.loading ? 'loading' : null}`
+  let classes = `list-view flex flex-column ${props.loading ? 'loading' : null}`
 
   return (
     <div className={classes}>{props.children}</div>
