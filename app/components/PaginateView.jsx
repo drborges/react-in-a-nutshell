39be3971362dc4f4e1e6b304d18336119d5843c8
Diego Borges <drborges.cic@gmail.com>
2016-10-16 12:34:07 -0200

    Remove needless paeseInt call on props.page

--- app/components/PaginateView.jsx
+++ app/components/PaginateView.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 const PaginateView = (props) => {
-  const currentPage = parseInt(props.page)
-  const next = () => { props.onNextPage(currentPage + 1) }
-  const prev = () => { if (currentPage > 1) props.onPreviousPage(currentPage - 1) }
+  const next = () => { props.onNextPage(props.page + 1) }
+  const prev = () => { if (props.page > 1) props.onPreviousPage(props.page - 1) }
   const goto = (index) => () => { props.onGotoPage(index) }
 
   return (
     <div className="paginate-view flex flex-row flex-center">
       <div>
         <button className="paginate-prev" onClick={prev}>&#10094;</button>
-        <button className='paginate-item' onClick={goto(currentPage)}>{currentPage}</button>
+        <button className='paginate-item' onClick={goto(props.page)}>{props.page}</button>
         <button className="paginate-next" onClick={next}>&#10095;</button>
       </div>
     </div>
