0f4cdcc29eb0e413ccc5e24e722e1ae1ec56645b
Diego Borges <drborges.cic@gmail.com>
2016-10-18 21:31:52 -0200

    Set up boilerplate project structure

--- app/index.js
+++ app/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import UserReposContainer from 'app/containers/UserReposContainer'
 
-// Add the corresponding css inline in the HTML, avoiding multiple network calls
+// This import tells Webpack to add the CSS code inline in the HTML
+// avoiding multiple network calls, one for fetching the index.html
+// and another for fetching the CSS file.
 import 'app/assets/css/styles.scss'
 
-const body =
-  <div className="flex flex-row flex-wrap">
-    <UserReposContainer owner="evancz" tabIndex="1" className="margin-sm flex-grow-even" />
-    <UserReposContainer owner="powerhome" tabIndex="2" className="margin-sm flex-grow-even" />
-    <UserReposContainer owner="drborges" tabIndex="3" className="margin-sm flex-grow-even" />
-  </div>
+const style = { fontWeight: 'bold' }
+const body = <h1 style={style}>Hello, Power!</h1>
 
 ReactDOM.render(body, document.getElementById('app'))
